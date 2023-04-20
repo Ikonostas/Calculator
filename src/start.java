36ce792ee0e93fa71d3a17d0bc7039b696f565fb
Ikonostas <Alex-jks1@mail.ru>
2023-04-21 02:39:46 +0300

    created Jframe

--- src/start.java
+++ src/start.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class start {
+package LowCool;
+
+import javax.swing.*;
+
+public class start {
+    private JFrame window;
+    public start(){
+        window = new JFrame("Calculator");
+        window.setSize(280, 350);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new start();
+            }
+        });
+    }
 }
